4eb9485045d860a52acc142ed97330a6959d0a82
Chrisctr <credfern96@gmail.com>
2020-09-21 22:26:46 +0100

    Trying something else

--- src/main/scala/com/qa/ims/configuration/MongoConfiguration.scala
+++ src/main/scala/com/qa/ims/configuration/MongoConfiguration.scala
@@ -23,80 +23,62 @@ import scala.collection.JavaConverters._
 import scala.concurrent.duration.Duration
 
 object MongoConfiguration {
-/*
-  val mongoClient: MongoClient = MongoClient("mongodb://localhost:27017")
 
-  val database: MongoDatabase = mongoClient.getDatabase("DbIMS")
+  val uri: String = "mongodb://localhost:27017"
+  System.setProperty("org.mongodb.async.type", "netty")
+  val client: MongoClient = MongoClient(uri)
+  val db: MongoDatabase = client.getDatabase("DbIMS")
+
+  def connection7(driver: AsyncDriver): Future[MongoConnection] = for {
+    parsedUri <- MongoConnection.fromString(uri)
+    con <- driver.connect(parsedUri)
+  } yield con
 
 
-  val collection: MongoCollection[Document] = database.getCollection("customer")
+  def troubleshootAuth() = {
+    val strictUri = "mongodb://localhost:27017"
+    val dbname = "DbIMS"
+    val user = "root"
+    val pass = "root"
+    val driver = AsyncDriver()
 
-
-
-  /// Create
-  try {
-    collection.insertOne(doc)
-    println("Document successfully inserted into customer collection")
-  } catch {
-    case e: Exception => println("Failed to insert document into customer collection")
-  }
-*/
-
-  val mongoUri = "mongodb://localhost:27017/mydb?authMode=scram-sha1"
-
-  import ExecutionContext.Implicits.global // use any appropriate context
-
-  // Connect to the database: Must be done only once per application
-  val driver = AsyncDriver()
-  val parsedUri = MongoConnection.fromString(mongoUri)
-
-  // Database and collections: Get references
-  val futureConnection = parsedUri.flatMap(driver.connect(_))
-  def db1: Future[DB] = futureConnection.flatMap(_.database("DbIMS"))
-  def personCollection = db1.map(_.collection("customer"))
-
-  // Write Documents: insert or update
-
-  implicit def personWriter: BSONDocumentWriter[Customer] = Macros.writer[Customer]
-  // or provide a custom one
-
-  // use personWriter
-  val doc: Document = Document("_id" -> 0, "name" -> "MongoDB", "type" -> "database",
-    "count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
-
-  def createPerson(collection: MongoCollection[Document]): Future[Unit] =
-    collection.flatMap(collection.insertOne(doc).map(_ => {}))
-
-  def updatePerson(customer: Customer): Future[Int] = {
-    val selector = document(
-      "firstName" -> customer.firstName,
-      "lastName" -> customer.lastName
-    )
-
-    // Update the matching person
-    personCollection.flatMap(_.updateOne(selector, person).map(_.n))
+    driver.connect(strictUri).flatMap {
+      _.authenticate(dbname, user, pass)
+    }.onComplete {
+      case res => println(s"Auth: $res")
+    }
   }
 
-  implicit def personReader: BSONDocumentReader[Customer] = Macros.reader[Customer]
-  // or provide a custom one
+  troubleshootAuth()
 
-  def findPersonByAge(age: Int): Future[List[Customer]] =
-    personCollection.flatMap(_.find(document("age" -> age)). // query builder
-      cursor[Customer](). // using the result cursor
-      collect[List](-1, Cursor.FailOnError[List[Customer]]()))
-  // ... deserializes the document using personReader
+  println(troubleshootAuth)
 
 
-
-  // Custom persistent types
-  case class Customer(firstName: String, lastName: String, age: Int)
-
-  /// Read
-  // Count
+  def dbFromConnection(connection: MongoConnection): Future[BSONCollection] =
+    connection.database("DbIMS").
+      map(_.collection("customers"))
 
 
 
-  println(s"total # of documents after inserting 100 small ones (should be 101):  ${insertAndCount.headResult()}")
+
+  val document1 = BSONDocument(
+    "firstName" -> "Chris",
+    "lastName" -> "Red",
+    "age" -> 24)
+
+  def testInsert(personColl: BSONCollection) = {
+    val future2 = personColl.insert.one(document1)
+
+    future2.onComplete {
+      case Failure(e) => throw e
+      case Success(writeResult) => {
+        println(s"successfully inserted document: $writeResult")
+      }
+    }
+  }
+
+
+
 
 
 
