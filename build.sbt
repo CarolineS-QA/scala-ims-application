27f75b78f26fc0b1a5af7eb926a674e425f7802e
Chrisctr <credfern96@gmail.com>
2020-09-21 20:43:54 +0100

    Trying original mongo connection method again

--- build.sbt
+++ build.sbt
@@ -6,5 +6,6 @@ scalaVersion := "2.13.3"
 
 libraryDependencies ++= Seq(
   "org.reactivemongo" %% "reactivemongo" % "1.0.0",
+  "org.mongodb.scala" %% "mongo-scala-driver" % "4.1.0-beta2",
   "org.slf4j" % "slf4j-log4j12" % "1.7.25"
 )
--- src/main/scala/com/qa/ims/configuration/MongoConfiguration.scala
+++ src/main/scala/com/qa/ims/configuration/MongoConfiguration.scala
@@ -1,17 +1,48 @@
 package com.qa.ims.configuration
 
+import java.util.concurrent.TimeUnit
+
 import reactivemongo.api.bson.collection.BSONCollection
 
-import scala.concurrent.{ExecutionContext, Future}
-import reactivemongo.api.{AsyncDriver, Cursor, DB, MongoConnection}
+import scala.concurrent.{Await, ExecutionContext, Future}
+import reactivemongo.api.{AsyncDriver, Cursor, DB, MongoConnection, MongoConnectionOptions}
 import reactivemongo.api.bson.{BSONDocument, BSONDocumentReader, BSONDocumentWriter, Macros, document}
 import reactivemongo.api.commands.WriteResult
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
+import org.mongodb.scala._
+import org.mongodb.scala.model.Aggregates._
+import org.mongodb.scala.model.Filters._
+import org.mongodb.scala.model.Projections._
+import org.mongodb.scala.model.Sorts._
+import org.mongodb.scala.model.Updates._
+import org.mongodb.scala.model._
+
+import scala.collection.JavaConverters._
+import scala.concurrent.duration.Duration
 
 object MongoConfiguration {
-  val mongoUri: String = "mongodb://root:root@clusterims-shard-00-00.tejxf.mongodb.net:27017,clusterims-shard-00-01.tejxf.mongodb.net:27017,clusterims-shard-00-02.tejxf.mongodb.net:27017/DbIMS?ssl=true&replicaSet=atlas-15lq0w-shard-0&authSource=admin&retryWrites=true&w=majority"
-  System.setProperty("org.mongodb.async.type", "netty")
+/*
+  val mongoClient: MongoClient = MongoClient("mongodb://localhost:27017")
+
+  val database: MongoDatabase = mongoClient.getDatabase("DbIMS")
+
+
+  val collection: MongoCollection[Document] = database.getCollection("customer")
+
+
+
+  /// Create
+  try {
+    collection.insertOne(doc)
+    println("Document successfully inserted into customer collection")
+  } catch {
+    case e: Exception => println("Failed to insert document into customer collection")
+  }
+*/
+
+  val mongoUri = "mongodb://localhost:27017/mydb?authMode=scram-sha1"
 
   import ExecutionContext.Implicits.global // use any appropriate context
 
@@ -22,28 +53,51 @@ object MongoConfiguration {
   // Database and collections: Get references
   val futureConnection = parsedUri.flatMap(driver.connect(_))
   def db1: Future[DB] = futureConnection.flatMap(_.database("DbIMS"))
+  def personCollection = db1.map(_.collection("customer"))
 
-  def personCollection = db1.map(_.collection("DbIMS"))
+  // Write Documents: insert or update
 
-  val document1 = BSONDocument(
-    "firstName" -> "Chris",
-    "lastName" -> "Red",
-    "age" -> 24)
+  implicit def personWriter: BSONDocumentWriter[Customer] = Macros.writer[Customer]
+  // or provide a custom one
 
-  def insertDoc1(coll: BSONCollection, doc: BSONDocument): Future[Unit] = {
-    val writeRes: Future[WriteResult] = coll.insert(document1)
+  // use personWriter
+  val doc: Document = Document("_id" -> 0, "name" -> "MongoDB", "type" -> "database",
+    "count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
 
-    writeRes.onComplete { // Dummy callbacks
-      case Failure(e) => e.printStackTrace()
-      case Success(writeResult) =>
-        println(s"successfully inserted document with result: $writeResult")
-    }
+  def createPerson(collection: MongoCollection[Document]): Future[Unit] =
+    collection.flatMap(collection.insertOne(doc).map(_ => {}))
 
-    writeRes.map(_ => {}) // in this example, do nothing with the success
+  def updatePerson(customer: Customer): Future[Int] = {
+    val selector = document(
+      "firstName" -> customer.firstName,
+      "lastName" -> customer.lastName
+    )
+
+    // Update the matching person
+    personCollection.flatMap(_.updateOne(selector, person).map(_.n))
+  }
+
+  implicit def personReader: BSONDocumentReader[Customer] = Macros.reader[Customer]
+  // or provide a custom one
+
+  def findPersonByAge(age: Int): Future[List[Customer]] =
+    personCollection.flatMap(_.find(document("age" -> age)). // query builder
+      cursor[Customer](). // using the result cursor
+      collect[List](-1, Cursor.FailOnError[List[Customer]]()))
+  // ... deserializes the document using personReader
 
 
 
-  println(parsedUri)
+  // Custom persistent types
+  case class Customer(firstName: String, lastName: String, age: Int)
+
+  /// Read
+  // Count
+
+
+
+  println(s"total # of documents after inserting 100 small ones (should be 101):  ${insertAndCount.headResult()}")
+
 
 
 }
