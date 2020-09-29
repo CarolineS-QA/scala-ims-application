344a9bc2fac66834a4e23b6dbcd87be79bbb5518
Chrisctr <credfern96@gmail.com>
2020-09-29 11:47:08 +0100

    Fixing IntelliJ bug

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,46 +1,15 @@
 package controllers
 
-import configuration.MongoConfiguration
-import configuration.MongoConfiguration.customerCollection
-import javax.inject.Inject
-import model.CustomerModel
-
-import scala.concurrent.{ExecutionContext, Future}
-import play.api.Logger
-import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, Request}
-import play.api.libs.functional.syntax._
-import play.api.libs.json.{JsArray, JsNumber, JsObject, JsString, JsValue, Json, Reads, __}
-import reactivemongo.api.bson.document
-import reactivemongo.api.commands.WriteResult
-
-import scala.language.postfixOps
-import scala.util.{Failure, Success}
-// Reactive Mongo imports
-import reactivemongo.api.Cursor
-import reactivemongo.api.ReadPreference
-
-import play.modules.reactivemongo.{ // ReactiveMongo Play2 plugin
-  MongoController,
-  ReactiveMongoApi,
-  ReactiveMongoComponents
-}
-
-// BSON-JSON conversions/collection
-import reactivemongo.play.json._, collection._
-
+import javax.inject._
+import play.api._
+import play.api.mvc._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
-
-class HomeController @Inject()(cc: ControllerComponents, val reactiveMongoApi: ReactiveMongoApi)
-  extends AbstractController(cc) with MongoController with ReactiveMongoComponents  with play.api.i18n.I18nSupport {
-
-  implicit def ec: ExecutionContext = cc.executionContext
-
-  MongoConfiguration
-
+@Singleton
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
   /**
    * Create an Action to render an HTML page.
@@ -49,23 +18,7 @@ class HomeController @Inject()(cc: ControllerComponents, val reactiveMongoApi: R
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-
-  def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+  def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
-  
-  def customerPage(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.customerPage())
-  }
-
-  def productPage(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.productPage())
-  }
-  
-  def orderPage(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.orderPage())
-  }
-
-
 }
-
