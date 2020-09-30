57b69b7f9c107818ca8f532ae7d6f3f3c394850f
Chrisctr <credfern96@gmail.com>
2020-09-30 10:27:23 +0100

    Changed product list to tuple for now, still trying to get a list of tuples to work

--- app/controllers/OrderForm.scala
+++ app/controllers/OrderForm.scala
@@ -1,24 +1,26 @@
 package controllers
 
 
-import play.api.data.Form
-import play.api.data.Forms.{bigDecimal, list, longNumber, mapping, text}
+import play.api.data.{Form, Mapping}
+import play.api.data.Forms.{bigDecimal, list, longNumber, mapping, text, tuple}
 import play.api.libs.json.{Json, OWrites}
 
-case class OrderForm(username: String, products: List[ProductListForm], date: String, totalPrice: BigDecimal)
+case class OrderForm(username: String, products: (String, Long), date: String, totalPrice: BigDecimal)
 case class ProductListForm(name: String, quantity: Long)
 
 object OrderForm {
   implicit val orderWrite: OWrites[OrderForm] = Json.writes[OrderForm]
   implicit val productListWrite: OWrites[ProductListForm] = Json.writes[ProductListForm]
 
+  val productListForm: Mapping[(String, Long)] = tuple(
+    "name" -> text,
+    "quantity" -> longNumber
+  )
+
   val form: Form[OrderForm] = Form(
     mapping(
       "username" -> text,
-      "products" -> list(mapping(
-        "name" -> text,
-        "quantity" -> longNumber
-      )(ProductListForm.apply)(ProductListForm.unapply)),
+      "products" -> productListForm,
       "date" -> text,
       "totalPrice" -> bigDecimal
     )(OrderForm.apply)(OrderForm.unapply)
